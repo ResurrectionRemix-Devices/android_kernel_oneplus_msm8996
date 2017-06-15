8fb0392853877fdfc48a21ffeadca6c07cc135c8
qiuchangping <qiuchangping@oneplus.net>
2017-06-15 16:52:27 +0800

    Synchronize codes for OxygenOS OP3_O2_Open_18/9
    The changes list:
    driver: input: touchscreen modify tp driver code for pickup gesture function
    ANDROID: dm verity fec: fix RS block calculation
    revert supporting for thermal control the config of cpuaffinity
    
    Change-Id: Ie670bf3916a6bcf63608eb794f0fd24326629cd2

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1418,6 +1418,8 @@ struct task_struct {
 	unsigned int policy;
 	int nr_cpus_allowed;
 	cpumask_t cpus_allowed;
+    int ams_policy;
+    struct list_head affinity_node;
 
 #ifdef CONFIG_PREEMPT_RCU
 	int rcu_read_lock_nesting;
@@ -3215,7 +3217,7 @@ struct migration_notify_data {
 
 extern struct atomic_notifier_head load_alert_notifier_head;
 
-extern long sched_setaffinity(pid_t pid, const struct cpumask *new_mask);
+extern long sched_setaffinity(pid_t pid, struct cpumask *new_mask);
 extern long sched_getaffinity(pid_t pid, struct cpumask *mask);
 
 #ifdef CONFIG_CGROUP_SCHED
